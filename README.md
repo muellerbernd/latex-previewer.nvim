d6038baf944d47e800e900f9017745291a5f58d3
Bernd Müller <bernd@muellerbernd.de>
2022-06-13 17:50:57 +0200

    init

---
+++ README.md
@@ -0,0 +1,41 @@
+# latex-previewer Plugin for neovim
+
+## latexmk setup
+* change your latexmk config *$HOME/.latexmkrc*
+```conf
+$dvi_previewer = 'start xdvi -watchfile 1.5';
+$ps_previewer  = 'start gv --watch';
+$pdf_previewer = 'start zathura';
+```
+* set zathura as default viewer
+
+## zathura setup
+* change your zathura config *$HOME/.config/zathura/zathurarc*
+```conf
+set synctex true
+set synctex-editor-command "nvr --remote-silent +%{line} %{input}"
+```
+* enable synctex set synctex-editor-command, needed for backward search (zathura to neovim)
+
+## backward search
+```zsh
+nvr --servername /tmp/nvimsocket NIKR_thesis.tex
+```
+* ctrl + left click in pdf opens source file
+
+## latex build command
+```zsh
+latexmk -pdf -interaction=nonstopmode -pvc -quiet -synctex=1 -shell-escape <my-file>.tex
+```
+* run latexmk
+* ```-pdf``` output pdf
+* ```-synctex=1``` enable synctex
+* ```-pvc``` continuously checking all input files for changes and re-compile the whole document if needed and always display the result
+* more information [here](https://mg.readthedocs.io/latexmk.html)
+
+## magic comments
+* add the following to your latex files in the very first line
+```latex
+% !TEX root = <my_main_file.tex>
+```
+* you need to set the path to your main latex file, so that the plugin can show the correct preview
---
+++ lua/latex-previewer.lua
@@ -0,0 +1,36 @@
+local function get_first_line()
+    -- test
+    local tbl = vim.api.nvim_buf_get_lines(0, 0, 1, false)
+    local first_line = tbl[1]
+    if tbl[0] ~= nil then
+        first_line = tbl[0]
+    else
+        first_line = tbl[1]
+    end
+    -- print(vim.pretty_print(first_line))
+    return first_line
+end
+
+local function preview()
+    if vim.bo.filetype == "tex" or vim.bo.filetype == "plaintex" then
+        local first_line = get_first_line()
+        local start_idx = string.find(first_line, "<")
+        local end_idx = string.find(first_line, ">")
+        if start_idx ~= nil and end_idx ~= nil then
+            local name_main_tex = string.sub(first_line, start_idx + 1,
+                                             end_idx - 1 - 4)
+            local pdf_name = name_main_tex .. ".pdf"
+            local r, c = unpack(vim.api.nvim_win_get_cursor(0))
+            local position = r .. ":" .. c .. ":" .. vim.fn.expand("%:p")
+            local command = string.format(
+                                "zathura -x 'nvr --remote +%%{line} %%{input}' --synctex-forward %s %s",
+                                position, pdf_name)
+            vim.fn.jobstart(command)
+        else
+            print("insert magic comment to this file")
+            print(
+                "% !TEX root = <~/Desktop/GitProjects/MA-Thesis/thesis/NIKR_thesis.tex>")
+        end
+    end
+end
+return {get_first_line = get_first_line, preview = preview}
